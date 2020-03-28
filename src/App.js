02758c47e2ef6a58ffecc5babd5a64856b9f8985
Anjali Hariharan <anjali@cpcdiagnostics.in>
2020-03-28 19:51:43 +0530

    create a list and add tasks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
 import React from "react";
 import TodoList from "./components/TodoList";
-import { Button, Dropdown, Input } from "semantic-ui-react";
+import { Divider, Dropdown } from "semantic-ui-react";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: {},
-      currentTask: "",
-      options: [],
-      currentOption: ""
+      options: [{ text: "React", value: "React" }],
+      currentOption: "React"
     };
   }
-
-  handleTaskInputChange = event => {
-    this.setState({ currentTask: event.target.value });
-  };
-
-  handleAddTask = event => {
-    if (!this.state.currentTask || !this.state.currentOption) {
-      return;
-    }
-    const newTask = {
-      id: new Date(),
-      text: this.state.currentTask,
-      active: true
-    };
-    this.setState(prevState => {
-      const selectedGroup = this.state.currentOption;
-      const allTasks = prevState.tasks;
-      if (selectedGroup && !allTasks.hasOwnProperty(selectedGroup)) {
-        allTasks[selectedGroup] = [];
-      }
-      allTasks[selectedGroup] = allTasks[selectedGroup].concat(newTask);
-      return {
-        tasks: allTasks,
-        currentTask: "",
-        currentOption: ""
-      };
-    });
-  };
-
-  handleKeyPress = event => {
-    if (event.key === "Enter") {
-      this.handleAddTask();
-    }
-  };
 
   handleGroupInputChange = (e, { value }) =>
     this.setState({ currentOption: value });
@@ -57,67 +21,33 @@ class App extends React.Component {
     }));
   };
 
-  handleCheckItem = itemId => {
-    const allTasks = this.state.tasks;
-    for (let group of Object.keys(allTasks)) {
-      allTasks[group] = allTasks[group].map(item => {
-        if (item.id === itemId) {
-          item.active = !item.active;
-        }
-        return item;
-      });
-    }
-    this.setState({ tasks: allTasks });
-  };
-
-  handleDeleteItem = itemId => {
-    const allTasks = this.state.tasks;
-    for (let group of Object.keys(allTasks)) {
-      allTasks[group] = allTasks[group].filter(item => {
-        if (item.id !== itemId) {
-          return true;
-        }
-        return false;
-      });
-    }
-    this.setState({ tasks: allTasks });
-  };
-
   render() {
-    console.log("rendering ", this.state);
+    console.log("rendering from app ", this.state);
     return (
-      <div>
+      <div className="todo-container">
         <h2>Todo</h2>
-        <div className="todo-container">
-          <Input action>
-            <input
-              name="task-input"
-              type="text"
-              placeholder="Add new task"
-              value={this.state.currentTask}
-              onChange={this.handleTaskInputChange}
-              onKeyPress={this.handleKeyPress}
-              autoFocus
-            ></input>
-            <Dropdown
-              placeholder="Add group"
-              search
-              selection
-              allowAdditions
-              value={this.state.currentOption}
-              options={this.state.options}
-              onAddItem={this.handleAddGroup}
-              onChange={this.handleGroupInputChange}
-            />
-            <Button name="add-task" type="submit" onClick={this.handleAddTask}>
-              Add
-            </Button>
-          </Input>
-          <TodoList
-            tasks={this.state.tasks}
-            checkItem={this.handleCheckItem}
-            deleteItem={this.handleDeleteItem}
+        <div className="list-container">
+          <Dropdown
+            placeholder="Add or Search List"
+            search
+            selection
+            allowAdditions
+            value={this.state.currentOption}
+            options={this.state.options}
+            onAddItem={this.handleAddGroup}
+            onChange={this.handleGroupInputChange}
           />
+          <Divider />
+          {this.state.currentOption ? (
+            <TodoList
+              lists={this.state.options}
+              selectedList={this.state.currentOption}
+            />
+          ) : (
+            <span style={{ fontStyle: "italic" }}>
+              Add or search a list to add your tasks
+            </span>
+          )}
         </div>
       </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,126 @@
 import React from "react";
-import TodoListGroup from "./TodoListGroup";
-import { List } from "semantic-ui-react";
+import TodoItem from "./TodoItem";
+import { Button, Input, List } from "semantic-ui-react";
 
 class TodoList extends React.Component {
-  render() {
-    const allTasks = this.props.tasks;
-    const listGroups = Object.keys(allTasks).map(group => {
-      if (allTasks[group].length > 0) {
-        return (
-          <TodoListGroup
-            tasks={allTasks[group]}
-            group={group}
-            checkItem={this.props.checkItem}
-            deleteItem={this.props.deleteItem}
-            key={group}
-          />
-        );
+  state = {
+    tasks: {
+      React: [
+        { id: new Date(), text: "Make a todo app", active: true },
+        { id: new Date(), text: "Read the docs", active: true },
+        { id: new Date(), text: "Do the course", active: true }
+      ]
+    },
+    latestTask: ""
+  };
+  handleTaskInputChange = event => {
+    this.setState({ latestTask: event.target.value });
+  };
+
+  handleAddTask = event => {
+    if (!this.state.latestTask) {
+      return;
+    }
+    const newTask = {
+      id: new Date(),
+      text: this.state.latestTask,
+      active: true
+    };
+    this.setState(prevState => {
+      const selectedList = this.props.selectedList;
+      const allTasks = prevState.tasks;
+      console.log(allTasks);
+      if (!allTasks.hasOwnProperty(selectedList)) {
+        allTasks[selectedList] = [];
       }
-      return false;
+      allTasks[selectedList] = allTasks[selectedList].concat(newTask);
+      console.log(allTasks);
+      return {
+        tasks: allTasks,
+        latestTask: ""
+      };
     });
-    return <List as="ol">{listGroups}</List>;
+  };
+
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
+      this.handleAddTask();
+    }
+  };
+
+  handleCheckItem = itemId => {
+    const allTasks = this.state.tasks;
+    for (let group of Object.keys(allTasks)) {
+      allTasks[group] = allTasks[group].map(item => {
+        if (item.id === itemId) {
+          item.active = !item.active;
+        }
+        return item;
+      });
+    }
+    this.setState({ tasks: allTasks });
+  };
+
+  handleDeleteItem = itemId => {
+    const allTasks = this.state.tasks;
+    for (let group of Object.keys(allTasks)) {
+      allTasks[group] = allTasks[group].filter(item => {
+        if (item.id !== itemId) {
+          return true;
+        }
+        return false;
+      });
+    }
+    this.setState({ tasks: allTasks });
+  };
+
+  render() {
+    console.log("rendering from todo list ", this.state);
+    const allTasks = this.state.tasks;
+    let displayTasks = [];
+    for (let list of Object.keys(allTasks)) {
+      if (list === this.props.selectedList) {
+        displayTasks = allTasks[list];
+      }
+    }
+
+    const listItems = displayTasks.map(item => {
+      return (
+        <TodoItem
+          task={item}
+          checkItem={this.handleCheckItem}
+          deleteItem={this.handleDeleteItem}
+          id={item.id}
+          key={item.id}
+        />
+      );
+    });
+
+    return (
+      <div className="task-container">
+        <Input action fluid>
+          <input
+            name="task-input"
+            type="text"
+            placeholder="Add new task"
+            value={this.state.latestTask}
+            onChange={this.handleTaskInputChange}
+            onKeyPress={this.handleKeyPress}
+            autoFocus
+          ></input>
+          <Button name="add-task" type="submit" onClick={this.handleAddTask}>
+            Add
+          </Button>
+        </Input>
+        {Object.keys(allTasks).length > 0 ? (
+          <List divided verticalAlign="middle">
+            {listItems}
+          </List>
+        ) : (
+          ""
+        )}
+      </div>
+    );
   }
 }
 
